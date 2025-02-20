26231751e1eb69cdea77a1b541444c3f8b561039
LSH <lsh2613@naver.com>
2025-02-21 01:37:45 +0900

    mod: 안쓰는 어노테이션 제거

--- src/main/java/com/redisson/pessimistic/PessimisticTicketService.java
+++ src/main/java/com/redisson/pessimistic/PessimisticTicketService.java
@@ -1,12 +1,9 @@
 package com.redisson.pessimistic;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PessimisticTicketService {
